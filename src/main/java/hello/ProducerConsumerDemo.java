243e6fc5749ca9e9d7f726a0ae641026d50fd88b
shenxiang <k7gxn56@gmail.com>
2020-03-08 00:11:21 +0800

    commit Demo

---
+++ src/main/java/hello/ProducerConsumerDemo.java
@@ -0,0 +1,70 @@
+package hello;
+
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ShareNum{
+
+    private Integer num = 0;
+
+    private Lock lock = new ReentrantLock();
+
+    private Condition condition = lock.newCondition();
+
+    public void increment(){
+        lock.lock();
+        try{
+            while (num != 0){
+                condition.await();
+            }
+            num++;
+            condition.signal();
+            System.out.println(Thread.currentThread().getName()+",+1,当前值:"+num);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void decrement(){
+        lock.lock();
+        try{
+            while (num == 0){
+                condition.await();
+            }
+            num--;
+            condition.signal();
+            System.out.println(Thread.currentThread().getName()+",-1,当前值:"+num);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+/**
+ * 初始值为0 两个用户，一个把他加1  一个把他减1   这个过程个重复5次
+ *
+ */
+public class ProducerConsumerDemo {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        ShareNum n = new ShareNum();
+        new Thread(() -> {
+            for (int i = 0;i < 5;i++){
+                n.increment();
+            }
+        },"t1").start();
+
+        new Thread(() -> {
+            for (int i = 0;i < 5;i++){
+                n.decrement();
+            }
+        },"t2").start();
+    }
+}
---
+++ src/main/java/hello/SyncAndReentrantLockDemo.java
@@ -0,0 +1,97 @@
+package hello;
+
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ShareResource{
+    private Integer n = 1;//1代表A 2代表B 3代表C
+    private Lock lock = new ReentrantLock();
+    private Condition a = lock.newCondition();
+    private Condition b = lock.newCondition();
+    private Condition c = lock.newCondition();
+
+    public void print2(){
+        lock.lock();
+        try{
+            while (n != 1){
+                a.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"打印1次");
+            System.out.println(Thread.currentThread().getName()+"打印2次");
+            n = 2;
+            b.signal();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void print3(){
+        lock.lock();
+        try{
+            while (n != 2){
+                b.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"打印1次");
+            System.out.println(Thread.currentThread().getName()+"打印2次");
+            System.out.println(Thread.currentThread().getName()+"打印3次");
+            n = 3;
+            c.signal();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void print4(){
+        lock.lock();
+        try{
+            while (n != 3){
+                c.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"打印1次");
+            System.out.println(Thread.currentThread().getName()+"打印2次");
+            System.out.println(Thread.currentThread().getName()+"打印3次");
+            System.out.println(Thread.currentThread().getName()+"打印4次");
+            n = 1;
+            a.signal();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+}
+
+/**
+ * 3个线程A B C,交替执行A 打印2次,B打印3次,C打印4次 ,重复3遍
+ * 检查  执行 通知
+ */
+public class SyncAndReentrantLockDemo {
+
+    public static void main(String[] args){
+        ShareResource shareResource = new ShareResource();
+
+        new Thread(() -> {
+            for (int i = 0;i < 3;i++) {
+                shareResource.print2();
+            }
+        },"A").start();
+
+        new Thread(() -> {
+            for (int i = 0;i < 3;i++) {
+                shareResource.print3();
+            }
+        },"B").start();
+
+        new Thread(() -> {
+            for (int i = 0;i < 3;i++) {
+                shareResource.print4();
+            }
+        },"C").start();
+    }
+}
